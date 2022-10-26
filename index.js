593e708331bc1369370ef8e6ff66fc738caf7061
khsultana <khsultana1995@gmail.com>
2022-10-27 02:14:33 +0600

    data load

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const cors = require("cors");
+
+app.use(cors());
+
 const courseCategory = require("./data/courseCategory.json");
 app.get("/course-categories", (req, res) => {
   res.send(courseCategory);
