d113401e33e8469620cf5387442801f39cd10f15
khsultana <khsultana1995@gmail.com>
2022-10-28 00:09:37 +0600

    vercel updated

--- index.js
+++ index.js
@@ -32,5 +32,5 @@ app.get("/", (req, res) => {
   res.send("News API Running");
 });
 app.listen(port, () => {
-  console.log(`News API listening on port ${port}`);
+  console.log(` API listening on port ${port}`);
 });
