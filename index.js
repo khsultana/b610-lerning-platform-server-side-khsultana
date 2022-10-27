9acedca063a6ddce55d8e00f5fdf52c0f744a4f4
khsultana <khsultana1995@gmail.com>
2022-10-27 20:06:45 +0600

    data details load

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ app.get("/course-categories", (req, res) => {
   res.send(courseCategory);
 });
 
+const courseDetails = require("./data/courseDetails.json");
+app.get("/course-Details", (req, res) => {
+  res.send(courseDetails);
+});
+
+app.get("/course-Details/:id", (req, res) => {
+  const id = req.params.id;
+  const course = courseDetails.find((c) => c._id == id);
+  res.send(course);
+});
+
+app.get("/course_Id/:id", (req, res) => {
+  const id = req.params.id;
+  const course_Id = courseDetails.filter((c) => c.course_Id == id);
+  res.send(course_Id);
+});
+
 app.get("/", (req, res) => {
   res.send("News API Running");
 });
